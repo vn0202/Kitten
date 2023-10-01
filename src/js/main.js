861caffe31ac885ef88a792f2423f7e88daf28a7
vn0202 <nghiavuxp0202@gmail.com>
2023-10-01 16:37:28 +0700

    add video

--- src/js/main.js
+++ src/js/main.js
@@ -8,9 +8,9 @@ function move_scroll(){
         const element = followers[index];
         let rect = element.getBoundingClientRect();
         console.log(rect.y, window.innerHeight - 300);
-      if( rect.y < window.innerHeight - 300 && rect.y > window.innerHeight - 450)
+      if( rect.y < window.innerHeight - 300 && rect.y > window.innerHeight - 600)
     {
-          element.style.transform ="scale(1.5)";
+          element.style.transform ="scale(1.3) rotate(40deg)";
           element.style.fill = "white";
     }
     else{
@@ -25,3 +25,11 @@ function move_scroll(){
 }
 
 document.addEventListener('scroll', move_scroll);
+
+// preload video
+document.addEventListener('load', () => {
+    const myVideo = document.getElementById('my-video');
+    myVideo.preload;
+
+})
+
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#E53988',
+        secondary: "#1ED6FF",
+      },
+    },
   },
   plugins: [],
 }
